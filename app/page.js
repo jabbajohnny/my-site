a4f52a1281bdc8159d55045b94b4711833ce4aa4
JabbaJohnny <46490740+JabbaJohnny@users.noreply.github.com>
2023-07-19 18:20:12 +0200

    Init commit

---
+++ app/page.js
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <main className="mx-40">
+      <div className="font-bold underline">
+        Hello! This is my website.
+      </div>
+    </main>
+  )
+}
