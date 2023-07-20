d8b0f7b791cf707aafb90c28a0fff99fdaf2a407
JabbaJohnny <46490740+JabbaJohnny@users.noreply.github.com>
2023-07-20 20:18:05 +0200

    Add profile card

---
+++ app/about/page.js
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <main className="mx-40">
+      <div className="font-bold underline">
+        Some information about me.
+      </div>
+    </main>
+  )
+}
--- app/page.js
+++ app/page.js
@@ -1,10 +1,27 @@
 
 export default function Home() {
   return (
-    <main className="mx-40">
+    <main className="mx-40 ">
       <div className="font-bold underline">
         Hello! This is my website.
       </div>
+      <ProfileCard/>
+
     </main>
+  )
+}
+
+export function ProfileImage() {
+  return (
+    <img className="w-1/6 rounded-full" src="/ryo.jpg"></img>
+  )
+}
+
+export function ProfileCard() {
+  return (
+    <div className=" content-center flex w-full justify-center">
+      <ProfileImage/>
+      <p className="w-1/4">jabbajohnny</p>
+    </div>
   )
 }
