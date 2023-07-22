70990a97fc8d0bc782595a78ba65f4e6182f8f9d
JabbaJohnny <46490740+JabbaJohnny@users.noreply.github.com>
2023-07-22 20:23:56 +0200

    Add shadow to profile picture

--- app/page.js
+++ app/page.js
@@ -12,13 +12,15 @@ export default function Home() {
 
 export function ProfileImage() {
   return (
-    <img className="w-80 h-80 rounded-full flex-shrink-0 m-2 " src="/ryo.jpg"></img>
+    <div className=" shadow-xl w-80 h-80 rounded-full flex-shrink-0 m-2 overflow-hidden">
+    <img className="w-full h-full" src="/ryo.jpg"></img>
+    </div>
   )
 }
 
 export function ProfileCard() {
   return (
-    <div className=" shadow-xl shadow-inner  border-solid border-8 m-auto content-center flex w-3/5 h-96 justify-left bg-gradient-to-r from-orange-500 to-violet-500">
+    <div className="  shadow-xl shadow-inner  border-solid border-8 m-auto content-center flex w-3/5 h-96 justify-left bg-gradient-to-r from-orange-500 to-violet-500">
       <ProfileImage/>
       <p className="w-1/4 h-1/4 flex-shrink-0  w-1/2 h-1/3 text-4xl text-center mt-4">jabbajohnny</p>
     </div>
