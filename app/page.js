57c6e81c01278dc054beb7d108fec9d33a92827d
JabbaJohnny <46490740+JabbaJohnny@users.noreply.github.com>
2023-07-22 18:52:11 +0200

    Resize name

--- app/page.js
+++ app/page.js
@@ -20,7 +20,7 @@ export function ProfileCard() {
   return (
     <div className=" m-auto content-center flex w-3/5 h-96 justify-left bg-gradient-to-r from-orange-500 to-violet-500">
       <ProfileImage/>
-      <p className="w-1/4 h-1/4 flex-shrink-0">jabbajohnny</p>
+      <p className="w-1/4 h-1/4 flex-shrink-0  w-1/2 h-1/3 text-4xl text-center mt-4">jabbajohnny</p>
     </div>
   )
 }
